bb2596d55c4259184e9cddcaa4db9b0bfd380eb9
chandru12588 <chandru.jerry@gmail.com>
2026-01-04 23:57:40 +0530

    updatedfirebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,14 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAYyNmOPz2YOjiKiaODE707CrOa5Lz5Q-8",
-  authDomain: "myblogportfolio-5eaff.firebaseapp.com",
-  projectId: "myblogportfolio-5eaff",
-  storageBucket: "myblogportfolio-5eaff.firebasestorage.app",
-  messagingSenderId: "112915525424",
-  appId: "1:112915525424:web:99f25c8aa2b804d07ea2f5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
