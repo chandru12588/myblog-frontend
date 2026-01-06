1c248120b6baf195c5a1669d35a8001bbcc8e9fd
chandru12588 <chandru.jerry@gmail.com>
2026-01-07 00:52:00 +0530

    updated api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,23 @@
 import axios from "axios";
+import { auth } from "../config/firebase";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+/* ================= AUTH INTERCEPTOR ================= */
+api.interceptors.request.use(
+  async (config) => {
+    const user = auth.currentUser;
+
+    if (user) {
+      const token = await user.getIdToken();
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
